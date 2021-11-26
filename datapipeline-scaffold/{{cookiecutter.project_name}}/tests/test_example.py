7f2d19c2ab4d1940db705665afd2095da4bb0865
gmodena <gmodena@wikimedia.org>
2021-11-26 11:27:21 +0100

    Rename cookiecutter

--- datapipeline-scaffold/{{cookiecutter.project_name}}/tests/test_example.py
+++
@@ -1,9 +0,0 @@
-from conftest import assert_shallow_equals
-from pyspark import Row
-
-
-def test_dataframe_equals(spark_session, data):
-    expected_df = spark_session.createDataFrame(
-        [("Alice", 1), ("Bob", 2), ("Eve", 3)], ["name", "age"]
-    )
-    assert_shallow_equals(data, expected_df)
