102d68453aba61ca9b6eba248ec5538d6fd45459
gmodena <gmodena@wikimedia.org>
2021-11-30 21:39:22 +0100

    Add docstrings to test boilerplate.

--- datapipeline-scaffold/{{cookiecutter.pipeline_slug}}/pyspark/tests/test_example.py
+++ datapipeline-scaffold/{{cookiecutter.pipeline_slug}}/pyspark/tests/test_example.py
@@ -2,10 +2,16 @@
 A boilerplate test suite for {{cookiecutter.pipeline_name}}
 """
 from conftest import assert_shallow_equals
-from pyspark import Row
 
 
 def test_dataframe_equals(spark_session, data):
+    """
+    This is a boilerplate unit test.
+
+    assert_shallow_equals can be used
+    to test that the result of a transformation matches t
+    he expected output.
+    """
     expected_df = spark_session.createDataFrame(
         [("Alice", 1), ("Bob", 2), ("Eve", 3)], ["name", "age"]
     )
