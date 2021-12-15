429609d1406286b94ff5a4691298b7af3ad4be5e
Gabriele Modena <gmodena@wikimedia.org>
2021-12-15 17:53:43 +0100

    Renamed pipeline_slug to pipeline_directory

--- datapipeline-scaffold/{{cookiecutter.pipeline_slug}}/pyspark/src/transform.py
+++
@@ -1,9 +0,0 @@
-"""
-A boilerplate Spark job for {{cookiecutter.pipeline_name}}
-"""
-from pyspark.sql import SparkSession
-
-if __name__ == '__main__':
-    spark = SparkSession.builder.getOrCreate()
-    # ...
-    spark.stop()
